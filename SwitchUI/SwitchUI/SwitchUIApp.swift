f0a623af6c8b4b9e47a0bca3cc2d1756afc165d9
Roar Lochar <roar.lochar@tietoevry.com>
2024-02-02 14:11:39 +0100

    Added default Xcode project.

---
+++ SwitchUI/SwitchUI/SwitchUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchUIApp.swift
+//  SwitchUI
+//
+//  Created by Roar Lochar on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwitchUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
